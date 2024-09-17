2ca3b381deb9820f90e0ca6407fb99f8a4b25797
ahoang2502 <athoang252@gmail.com>
2024-09-17 14:58:09 +1000

    feat: user button && logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 "use client";
 
-import { useAuthActions } from "@convex-dev/auth/react";
-
 import { UserButton } from "@/components/UserButton";
 
 export default function Home() {
-  const { signOut } = useAuthActions();
-
   return (
     <div>
       <UserButton />
--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Loader } from "lucide-react";
+import { useAuthActions } from "@convex-dev/auth/react";
+import { Loader, LogOut } from "lucide-react";
 
 import { useCurrentUser } from "@/features/auth/hooks/useCurrentUser";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
@@ -12,24 +13,34 @@ import {
 } from "./ui/dropdown-menu";
 
 export const UserButton = () => {
+  const { signOut } = useAuthActions();
   const { data, isLoading } = useCurrentUser();
 
   if (isLoading)
     return <Loader className="size-4 text-muted-foreground animate-spin" />;
 
-  if (data === null) return null;
+  if (!data) return null;
+
+  const { image, name, email } = data;
+
+  const avatarFallback = name!.charAt(0).toUpperCase();
 
   return (
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger className="outline-none relative">
         <Avatar className="size-10 hover:opacity-75 transition">
-          <AvatarImage />
-          <AvatarFallback></AvatarFallback>
+          <AvatarImage alt={name} src={image} />
+          <AvatarFallback className="bg-sky-500 text-white">
+            {avatarFallback}
+          </AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align="center" side="right" className="w-60">
-        <DropdownMenuItem></DropdownMenuItem>
+        <DropdownMenuItem onClick={() => signOut()} className="h-10">
+          <LogOut className="size-4 mr-2" />
+          Log out
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
