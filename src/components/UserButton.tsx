92e2e7cf9b3d3643c74fb5184d5a63a404b3c09c
ahoang2502 <athoang252@gmail.com>
2024-09-17 14:52:24 +1000

    feat: user api

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Loader } from "lucide-react";
+
+import { useCurrentUser } from "@/features/auth/hooks/useCurrentUser";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import {
   DropdownMenu,
@@ -9,21 +12,24 @@ import {
 } from "./ui/dropdown-menu";
 
 export const UserButton = () => {
+  const { data, isLoading } = useCurrentUser();
+
+  if (isLoading)
+    return <Loader className="size-4 text-muted-foreground animate-spin" />;
+
+  if (data === null) return null;
+
   return (
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger className="outline-none relative">
         <Avatar className="size-10 hover:opacity-75 transition">
           <AvatarImage />
-          <AvatarFallback>
-            
-          </AvatarFallback>
+          <AvatarFallback></AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
 
-      <DropdownMenuContent align='center' side="right" className="w-60">
-        <DropdownMenuItem>
-
-        </DropdownMenuItem>
+      <DropdownMenuContent align="center" side="right" className="w-60">
+        <DropdownMenuItem></DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
