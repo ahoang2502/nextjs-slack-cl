bf0fb49432916b6c0bc7d7b1b6953f6c5f0c4846
ahoang2502 <athoang252@gmail.com>
2024-09-18 21:54:42 +1000

    feat: useGetWorkspaces hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
 "use client";
 
+import { useEffect, useMemo } from "react";
+
 import { UserButton } from "@/components/UserButton";
 
+import { useGetWorkspaces } from "@/features/workspaces/api/useGetWorkspaces";
+
 export default function Home() {
-  
+  const { data, isLoading } = useGetWorkspaces();
+
+  const workspaceId = useMemo(() => data?.[0]?._id, [data]);
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    if (workspaceId) console.log("redirect to workspace");
+    else console.log("open creation modal");
+  }, [workspaceId, isLoading]);
+
   return (
     <div>
-
       <UserButton />
     </div>
   );
