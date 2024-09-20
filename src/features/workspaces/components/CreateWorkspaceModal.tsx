1dc48d8308fe4ecd90a650d6b48b700098e149df
ahoang2502 <athoang252@gmail.com>
2024-09-20 21:28:48 +1000

    feat: create-workspace api

--- src/features/workspaces/components/CreateWorkspaceModal.tsx
+++ src/features/workspaces/components/CreateWorkspaceModal.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from "next/navigation";
+
 import {
   Dialog,
   DialogContent,
@@ -6,12 +8,33 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { useCreateWorkspaceModal } from "../store/useCreateWorkspaceModal";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useCreateWorkspace } from "../api/useCreateWorkspace";
 
 export const CreateWorkspaceModal = () => {
+  const router = useRouter();
   const [open, setOpen] = useCreateWorkspaceModal();
+
+  const { mutate } = useCreateWorkspace();
 
   const handleClose = () => {
     setOpen(false);
+  };
+
+  const handleSubmit = () => {
+    mutate(
+      {
+        name: "Workspace 1",
+      },
+      {
+        onSuccess: (data) => {
+          router.push(`/workspaces/${data}`);
+        },
+        onError: () => {},
+        onSettled: () => {},
+      }
+    );
   };
 
   return (
@@ -20,6 +43,21 @@ export const CreateWorkspaceModal = () => {
         <DialogHeader>
           <DialogTitle>Add a workspace</DialogTitle>
         </DialogHeader>
+
+        <form className="space-y-4">
+          <Input
+            disabled={false}
+            value=""
+            required
+            autoFocus
+            minLength={3}
+            placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+          />
+
+          <div className="flex justify-end">
+            <Button disabled={false}>Create</Button>
+          </div>
+        </form>
       </DialogContent>
     </Dialog>
   );
