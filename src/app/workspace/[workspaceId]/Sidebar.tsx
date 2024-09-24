4974d47174b4c67326235582e0154d975d0d203a
ahoang2502 <athoang252@gmail.com>
2024-09-24 15:47:12 +1000

    feat: workspace page: sidebar

--- src/app/workspace/[workspaceId]/Sidebar.tsx
+++ src/app/workspace/[workspaceId]/Sidebar.tsx
@@ -1,10 +1,24 @@
+import { Bell, Home, MessageCircle, MoreHorizontal } from "lucide-react";
+import { usePathname } from "next/navigation";
+
 import { UserButton } from "@/components/UserButton";
+import { SidebarButton } from "./SidebarButton";
 import { WorkspaceSwitcher } from "./WorkspaceSwitcher";
 
 export const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <aside className="w-[70px] h-full bg-[#611e69] flex flex-col gap-y-4 items-center pt-[9px] pb-[4px]">
       <WorkspaceSwitcher />
+      <SidebarButton
+        icon={Home}
+        label="Home"
+        isActive={pathname.includes("/workspace")}
+      />
+      <SidebarButton icon={MessageCircle} label="DMs" />
+      <SidebarButton icon={Bell} label="Activity" />
+      <SidebarButton icon={MoreHorizontal} label="More" />
 
       <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
         <UserButton />
