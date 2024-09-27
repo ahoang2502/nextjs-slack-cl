651f9563f37aa59a1647226e2898e4085f78a5ba
ahoang2502 <athoang252@gmail.com>
2024-09-27 12:27:27 +1000

    feat: workspace sidebar: error & loading

---
+++ src/app/workspace/[workspaceId]/WorkspaceHeader.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const WorkspaceHeader = () => {
+  return (
+    <div>WorkspaceHeader</div>
+  )
+}
--- src/app/workspace/[workspaceId]/WorkspaceSidebar.tsx
+++ src/app/workspace/[workspaceId]/WorkspaceSidebar.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import { AlertTriangle, Loader } from "lucide-react";
+
+import { useCurrentMember } from "@/features/members/api/useCurrentMember";
+import { useGetWorkspace } from "@/features/workspaces/api/useGetWorkspace";
+import { useWorkspaceId } from "@/hooks/useWorkspaceId";
+import { WorkspaceHeader } from "./WorkspaceHeader";
 
 export const WorkspaceSidebar = () => {
+  const workspaceId = useWorkspaceId();
+
+  const { data: member, isLoading: memberLoading } = useCurrentMember({
+    workspaceId,
+  });
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({
+    id: workspaceId,
+  });
+
+  if (workspaceLoading || memberLoading)
+    return (
+      <div className="flex flex-col bg-[#86558d] h-full items-center justify-center">
+        <Loader className="size-5 animate-spin text-white" />
+      </div>
+    );
+
+  if (!workspace || !member)
+    return (
+      <div className="flex flex-col gap-y-2 bg-[#86558d] h-full items-center justify-center">
+        <AlertTriangle className="size-5 text-white" />
+
+        <p className="text-white text-sm">Workspace not found</p>
+      </div>
+    );
+
   return (
-    <div>WorkspaceSidebar</div>
-  )
-}
+    <div className="flex flex-col bg-[#86558d] h-full">
+      <WorkspaceHeader />
+    </div>
+  );
+};
