7a350a792b78198aa138d93a3b90ed37d8514cdd
ahoang2502 <athoang252@gmail.com>
2024-10-07 23:50:36 +1100

    feat: workspace: redirect to a channel

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,5 +1,64 @@
+"use client";
+
+import { Loader, TriangleAlert } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useMemo } from "react";
+
+import { useWorkspaceId } from "@/hooks/useWorkspaceId";
+
+import { useGetChannels } from "@/features/channels/api/useGetChannels";
+import { useCreateChannelModal } from "@/features/channels/store/useCreateChannelModal";
+import { useGetWorkspace } from "@/features/workspaces/api/useGetWorkspace";
+
 const WorkspaceIdPage = () => {
-  return <div>Id:</div>;
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
+  const [open, setOpen] = useCreateChannelModal();
+
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({
+    id: workspaceId,
+  });
+  const { data: channels, isLoading: channelsLoading } = useGetChannels({
+    workspaceId,
+  });
+
+  const channelId = useMemo(() => channels?.[0]._id, [channels]);
+
+  useEffect(() => {
+    if (workspaceLoading || channelsLoading || !workspace) return;
+
+    if (channelId)
+      router.push(`/workspace/${workspaceId}/channel/${channelId}`);
+    else if (!open) setOpen(true);
+  }, [
+    channelId,
+    workspaceLoading,
+    channelsLoading,
+    workspace,
+    open,
+    setOpen,
+    router,
+    workspaceId,
+  ]);
+
+  if (workspaceLoading || channelsLoading)
+    return (
+      <div className="h-full flex-1 flex items-center justify-center flex-col gap-2">
+        <Loader className="size-6 animate-spin text-muted-foreground" />
+      </div>
+    );
+
+  if (!workspace)
+    return (
+      <div className="h-full flex-1 flex items-center justify-center flex-col gap-2">
+        <TriangleAlert className="size-6 text-muted-foreground" />
+        <span className="text-sm text-muted-foreground">
+          Workspace not found
+        </span>
+      </div>
+    );
+
+  return null;
 };
 
 export default WorkspaceIdPage;
