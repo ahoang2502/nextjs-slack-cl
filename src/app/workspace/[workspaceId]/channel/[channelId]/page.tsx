befd297208ca3505b6049d27bf4a14b7a7ac67f6
ahoang2502 <athoang252@gmail.com>
2024-10-08 00:13:47 +1100

    feat: get channel by id

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,6 +1,33 @@
-import React from "react";
+"use client";
+
+import { Loader, TriangleAlert } from "lucide-react";
+
+import { useChannelId } from "@/hooks/useChannelId";
+
+import { useGetChannel } from "@/features/channels/api/useGetChannel";
 
 const ChannelIdPage = () => {
+  const channelId = useChannelId();
+
+  const { data: channel, isLoading: channelLoading } = useGetChannel({
+    id: channelId,
+  });
+
+  if (channelLoading)
+    return (
+      <div className="h-full flex-1 flex items-center justify-center">
+        <Loader className="size-5 animate-spin text-muted-foreground" />
+      </div>
+    );
+
+  if (!channel)
+    return (
+      <div className="h-full flex-1 flex flex-col gap-y-2 items-center justify-center">
+        <TriangleAlert className="size-5 text-muted-foreground" />
+        <span className="text-sm text-muted-foreground">Channel not found</span>
+      </div>
+    );
+
   return <div>
     
   </div>;
