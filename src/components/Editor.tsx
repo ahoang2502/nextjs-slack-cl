f0dc37995d3e6cdb0e0c17d5c8b6b99aafdca89a
ahoang2502 <athoang252@gmail.com>
2024-10-13 20:58:08 +1100

    feat: customize editor UI

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,7 +1,12 @@
+import { ImageIcon, Smile } from "lucide-react";
 import Quill, { QuillOptions } from "quill";
 import { useEffect, useRef } from "react";
+import { PiTextAa } from "react-icons/pi";
+import { MdSend } from "react-icons/md";
 
 import "quill/dist/quill.snow.css";
+import { Button } from "./ui/button";
+import { Hint } from "./Hint";
 
 const Editor = () => {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -31,6 +36,56 @@ const Editor = () => {
     <div className="flex flex-col">
       <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
         <div ref={containerRef} className="h-full ql-custom" />
+
+        <div className="flex px-2 pb-2 z-[5] ">
+          <Hint label="Hide formatting">
+            <Button
+              disabled={false}
+              size="iconSm"
+              variant="ghost"
+              onClick={() => {}}
+            >
+              <PiTextAa className="size-4 " />
+            </Button>
+          </Hint>
+
+          <Hint label="Emoji">
+            <Button
+              disabled={false}
+              size="iconSm"
+              variant="ghost"
+              onClick={() => {}}
+            >
+              <Smile className="size-4 " />
+            </Button>
+          </Hint>
+
+          <Hint label="Image">
+            <Button
+              disabled={false}
+              size="iconSm"
+              variant="ghost"
+              onClick={() => {}}
+            >
+              <ImageIcon className="size-4 " />
+            </Button>
+          </Hint>
+
+          <Button
+            disabled={false}
+            onClick={() => {}}
+            className="ml-auto bg-[#007a5a] hover:bg-[#007a5a]/80 text-white"
+            size="iconSm"
+          >
+            <MdSend className="size-4" />
+          </Button>
+        </div>
+      </div>
+
+      <div className="p-2 text-[10px] text-muted-foreground flex justify-end">
+        <p className="">
+          <strong>Shift + Return</strong> to add a new line
+        </p>
       </div>
     </div>
   );
