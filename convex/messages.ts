d311bbd5fe3ffd6d73ed002cb77f83f3d716a51c
ahoang2502 <athoang252@gmail.com>
2024-11-11 23:41:24 +1100

    fix: message updatedAt

--- convex/messages.ts
+++ convex/messages.ts
@@ -191,7 +191,6 @@ export const create = mutation({
       conversationId: _conversationId,
       workspaceId: args.workspaceId,
       parentMessageId: args.parentMessageId,
-      updatedAt: Date.now(),
     });
 
     return messageId;
