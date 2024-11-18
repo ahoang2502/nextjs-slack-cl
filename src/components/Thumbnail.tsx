dd180c2e9eb6d434aa39a265ecf32a00c1830896
ahoang2502 <athoang252@gmail.com>
2024-11-18 21:57:31 +1100

    feat: message image

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -2,7 +2,7 @@
 
 import { XIcon } from "lucide-react";
 
-import { Dialog } from "./ui/dialog";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 
 interface ThumbnailProps {
   url: string | null | undefined;
@@ -12,12 +12,24 @@ export const Thumbnail = ({ url }: ThumbnailProps) => {
   if (!url) return null;
 
   return (
-    <div className="relative overflow-hidden max-w-[360px] border rounded-lg my-2 cursor-zoom-in">
-      <img
-        src={url}
-        alt="Message image"
-        className="rounded-md object-cover size-full"
-      />
-    </div>
+    <Dialog>
+      <DialogTrigger>
+        <div className="relative overflow-hidden max-w-[360px] border rounded-lg my-2 cursor-zoom-in">
+          <img
+            src={url}
+            alt="Message image"
+            className="rounded-md object-cover size-full"
+          />
+        </div>
+      </DialogTrigger>
+
+      <DialogContent className="max-w-[800px] border-none bg-transparent p-0 shadow-none">
+        <img
+          src={url}
+          alt="Message image"
+          className="rounded-md object-cover size-full"
+        />
+      </DialogContent>
+    </Dialog>
   );
 };
