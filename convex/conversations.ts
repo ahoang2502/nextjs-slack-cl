acbe2b601b653433dbf79c0bab36e77761e18c6c
ahoang2502 <athoang252@gmail.com>
2024-12-30 22:55:19 +1100

    feat: DMs feature - get or create conversation hook & ui

--- convex/conversations.ts
+++ convex/conversations.ts
@@ -39,7 +39,7 @@ export const createOrGet = mutation({
       )
       .unique();
 
-    if (existingConversation) return existingConversation;
+    if (existingConversation) return existingConversation._id;
 
     const conversationId = await ctx.db.insert("conversations", {
       workspaceId: args.workspaceId,
@@ -47,9 +47,6 @@ export const createOrGet = mutation({
       memberTwoId: otherMember._id,
     });
 
-    const conversation = await ctx.db.get(conversationId);
-    if (!conversation) throw new Error("Conversation not found");
-
-    return conversation;
+    return conversationId;
   },
 });
--- src/app/workspace/[workspaceId]/member/[memberId]/page.tsx
+++ src/app/workspace/[workspaceId]/member/[memberId]/page.tsx
@@ -1,13 +1,56 @@
 "use client";
 
+import { AlertTriangle, Loader } from "lucide-react";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+
 import { useMemberId } from "@/hooks/useMemberId";
 import { useWorkspaceId } from "@/hooks/useWorkspaceId";
+import { Id } from "../../../../../../convex/_generated/dataModel";
+
+import { useCreateOrGetConversation } from "@/features/conversations/api/useCreateOrGetConversation";
 
 const MemberIdPage = () => {
   const workspaceId = useWorkspaceId();
   const memberId = useMemberId();
 
-  return <div></div>;
+  const [conversationId, setConversationId] =
+    useState<Id<"conversations"> | null>(null);
+
+  const { data, mutate, isPending } = useCreateOrGetConversation();
+
+  useEffect(() => {
+    mutate(
+      { workspaceId, memberId },
+      {
+        onSuccess(data) {
+          setConversationId(data);
+        },
+        onError(error) {
+          toast.error("Failed to create or get conversation");
+        },
+      }
+    );
+  }, [memberId, workspaceId, mutate]);
+
+  if (isPending)
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Loader className="size-6 text-muted-foreground animate-spin" />
+      </div>
+    );
+
+  if (!conversationId)
+    return (
+      <div className="h-full flex flex-col gap-y-2 items-center justify-center">
+        <AlertTriangle className="size-6 text-muted-foreground" />
+        <span className="text-sm text-muted-foreground">
+          Conversation not found
+        </span>
+      </div>
+    );
+
+  return <Conversation id={conversationId} />;
 };
 
 export default MemberIdPage;
