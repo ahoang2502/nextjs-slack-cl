5f60c8f6796bdb946bd529a2194a99975961e578
ahoang2502 <athoang252@gmail.com>
2024-12-30 23:16:39 +1100

    feat: DM conversation header

--- src/app/workspace/[workspaceId]/member/[memberId]/Conversation.tsx
+++ src/app/workspace/[workspaceId]/member/[memberId]/Conversation.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import { Loader } from "lucide-react";
 
-import { Id } from "../../../../../../convex/_generated/dataModel";
 import { useMemberId } from "@/hooks/useMemberId";
+import { Id } from "../../../../../../convex/_generated/dataModel";
+import { Header } from "./Header";
 
 import { useGetMember } from "@/features/members/api/useGetMember";
+import { useGetMessages } from "@/features/messages/api/useGetMessages";
 
 interface ConversationProps {
   id: Id<"conversations">;
@@ -15,6 +17,24 @@ export const Conversation = ({ id }: ConversationProps) => {
   const { data: member, isLoading: memberLoading } = useGetMember({
     id: memberId,
   });
+  const { results, status, loadMore } = useGetMessages({
+    conversationId: id,
+  });
 
-  return <div></div>;
+  if (memberLoading || status === "LoadingFirstPage")
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Loader className="size-6 text-muted-foreground animate-spin" />
+      </div>
+    );
+
+  return (
+    <div className="flex flex-col h-full">
+      <Header
+        memberName={member?.user.name}
+        memberImage={member?.user.image}
+        onClick={() => {}}
+      />
+    </div>
+  );
 };
